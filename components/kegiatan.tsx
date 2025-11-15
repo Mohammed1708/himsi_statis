57d85458a5ad2d7f25e18d44922a79b48e90e860
Muhammad17 <tufail.aljabry@gmail.com>
2025-11-16 00:01:24 +0700

    u

--- components/kegiatan.tsx
+++ components/kegiatan.tsx
@@ -15,19 +15,19 @@ export default function Kegiatan() {
       judul: "Seminar Tranformasi Digital",
       tanggal: "22 Mei 2025",
       deskripsi: "HIMSI Universitas Nusa Mandiri sukses menggelar seminar kolaboratif dengan NIC bertema “Transformasi Digital: Menyongsong Era Bisnis Berbasis Data dan AI”. Kegiatan ini menghadirkan narasumber inspiratif yang membagikan wawasan tentang pemanfaatan data dan AI untuk efisiensi dan inovasi bisnis, mengajak peserta memahami transformasi digital sebagai kunci menghadapi perkembangan industri. Acara berlangsung meriah dengan antusiasme peserta yang aktif berdiskusi, merefleksikan komitmen HIMSI dalam menyediakan wadah pengembangan pengetahuan yang relevan dengan perkembangan zaman.",
-      image: "/std.jpg",
+      image: "/seminar.jpg",
     },
     {
       judul: "Studi Banding HIMSI UNM X HIMASI STTNF",
       tanggal: "14 Juni 2025",
       deskripsi: "Dalam rangka mempererat silaturahmi dan meningkatkan kualitas organisasi, HIMSI UNM berkolaborasi dengan HIMASI STT NF menyelenggarakan Studi Banding bertema “Menguatkan Sinergi, Membangun Kolaborasi” pada 14 Juni 2025 di Depok. Kegiatan ini menjadi ajang strategis bagi kedua himpunan untuk saling berbagi pengalaman, bertukar ide, serta memperluas wawasan pengelolaan organisasi melalui diskusi, presentasi, dan forum interaktif, sehingga diharapkan dapat melahirkan inovasi kolaboratif dan memperkuat hubungan antarlembaga.",
-      image: "/stuban.jpg",
+      image: "/stubanHimsi.jpg",
     },
     {
       judul: "PMinKref HIMSI X HIMMA",
       tanggal: "4 Juli 2025",
       deskripsi: "HIMSI Universitas Nusa Mandiri berkolaborasi dengan HIMMA sukses menyelenggarakan MinKref (Mini Kreatif), sebuah workshop desain poster yang bertujuan mengembangkan potensi dan kreativitas mahasiswa. Kegiatan ini menjadi wadah bagi peserta untuk mempelajari dasar-dasar desain grafis hingga teknik praktis pembuatan poster dalam suasana yang hangat dan interaktif. Melalui workshop ini, peserta tidak hanya mendapat pengetahuan baru, tetapi juga kesempatan berkreasi langsung dan bertukar ide secara menyenangkan.",
-      image: "/pminkref.jpg",
+      image: "/minkref.jpg",
     },
   ]
 
